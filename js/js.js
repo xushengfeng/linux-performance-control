89e1ffdc5078a2d8c982684422929feb6fc185c4
xushengfeng <xushengfeng_zg@163.com>
2021-10-02 21:54:57 +0800

    :fire:删除无用文件

--- js/js.js
+++
@@ -1,8 +0,0 @@
-const ipc = require('electron').ipcRenderer
-
-const trayBtn = document.getElementById('put-in-tray')
-ipc.on('tray-removed', function (e) {
-  let myNotification = new Notification("测试标题", {
-    body: e,
-});
-})

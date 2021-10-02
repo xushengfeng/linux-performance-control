e8940700f14e88c45471be332ca0aa06d778f895
xushengfeng <xushengfeng_zg@163.com>
2021-10-02 21:40:42 +0800

    :sparkles:运行成功后有通知

--- js/js.js
+++ js/js.js
@@ -1,23 +1,8 @@
 const ipc = require('electron').ipcRenderer
 
 const trayBtn = document.getElementById('put-in-tray')
-let trayOn = false
-
-trayBtn.addEventListener('click', function (event) {
-  if (trayOn) {
-    trayOn = false
-    document.getElementById('tray-countdown').innerHTML = ''
-    ipc.send('remove-tray')
-  } else {
-    trayOn = true
-    const message = '再次点击示例按钮移除托盘.'
-    document.getElementById('tray-countdown').innerHTML = message
-    ipc.send('put-in-tray')
-  }
-})
-// 从图标上下文菜单中删除托盘
-ipc.on('tray-removed', function () {
-    trayOn = false
-    document.getElementById('tray-countdown').innerHTML = ''
-    ipc.send('remove-tray')
+ipc.on('tray-removed', function (e) {
+  let myNotification = new Notification("测试标题", {
+    body: e,
+});
 })
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, Menu, Tray } = require("electron");
+const { app, Menu, Tray, BrowserWindow,Notification } = require("electron");
+const path = require("path");
 
 let tray = null;
 app.whenReady().then(() => {
@@ -8,24 +9,21 @@ app.whenReady().then(() => {
             label: "均衡",
             type: "radio",
             click: () => {
-                b = run("sudo ./run.sh 00");
-                check(0,b);
+                run(0,"sudo ./run.sh 00");
             },
         },
         {
             label: "性能",
             type: "radio",
             click: function () {
-                b = run("sudo ./run.sh 01");
-                check(0,b);
+                run(0,"sudo ./run.sh 01");
             },
         },
         {
             label: "省电",
             type: "radio",
             click: function () {
-                b = run("sudo ./run.sh 02");
-                check(0,b);
+                run(0,"sudo ./run.sh 02");
             },
         },
         {
@@ -36,11 +34,9 @@ app.whenReady().then(() => {
             type: "checkbox",
             click: () => {
                 if (contextMenu.items[4].checked) {
-                    b = run("sudo ./run.sh 10");
-                    check(1,b);
+                    run(1,"sudo ./run.sh 10");
                 } else {
-                    b = run("sudo ./run.sh 11");
-                    check(1,b);
+                    run(1,"sudo ./run.sh 11");
                 }
             },
         },
@@ -49,11 +45,9 @@ app.whenReady().then(() => {
             type: "checkbox",
             click: function () {
                 if (contextMenu.items[5].checked) {
-                    b = run("sudo ./run.sh 20");
-                    check(2,b);
+                    run(2,"sudo ./run.sh 20");
                 } else {
-                    b = run("sudo ./run.sh 21");
-                    check(2,b);
+                    run(2,"sudo ./run.sh 21");
                 }
             },
         },
@@ -71,71 +65,75 @@ app.whenReady().then(() => {
     tray.setTitle("hi");
     tray.setContextMenu(contextMenu);
 
-    function check(n,x){
-        switch(n){
+    const { spawn } = require("child_process");
+
+    function run(n,c) {
+        const ls = spawn(c, {
+            encoding: "utf8",
+            cwd: process.cwd(), // 执行命令路径
+            shell: true, // 使用shell命令
+        });
+    
+        // 监听标准输出
+        ls.stdout.on("data", (data) => {
+            console.log(`${data}`);
+            check(n,`${data}`)
+            return `${data}`;
+        });
+    
+        // 监听标准错误
+        ls.stderr.on("data", (data) => {
+            show("失败");
+        });
+    
+        // 子进程关闭事件
+        ls.on("close", (code) => {
+            console.log(`子进程退出，退出码 ${code}`);
+        });
+    }
+
+    function check(n, x) {
+        x=x.match(/[0-9]x[0-9]/)?.[0]
+        switch (n) {
             case 0:
-                switch(x){
-                    case '0x0':
-                        contextMenu.items[0].checked = true
-                        show('均衡模式开启')
-                        break
-                    case '0x1':
-                        contextMenu.items[1].checked = true
-                        show('性能模式开启')
-                        break
-                    case '0x2':
-                        contextMenu.items[2].checked = true
-                        show('省电模式开启')
-                        break
+                switch (x) {
+                    case "0x0":
+                        contextMenu.items[0].checked = true;
+                        show("均衡模式开启");
+                        break;
+                    case "0x1":
+                        contextMenu.items[1].checked = true;
+                        show("性能模式开启");
+                        break;
+                    case "0x2":
+                        contextMenu.items[2].checked = true;
+                        show("省电模式开启");
+                        break;
                 }
-                break
+                break;
             case 1:
-                if(x=='0x0'){
-                    contextMenu.items[4].checked = false
-                }else{
-                    contextMenu.items[4].checked =true
-                    contextMenu.items[5].checked = false
+                if (x == "0x0") {
+                    contextMenu.items[4].checked = false;
+                } else {
+                    contextMenu.items[4].checked = true;
+                    contextMenu.items[5].checked = false;
                 }
-                break
+                break;
             case 2:
-                if(x=='0x0'){
-                    contextMenu.items[5].checked = false
-                }else{
-                    contextMenu.items[5].checked =true
-                    contextMenu.items[4].checked = false
+                if (x == "0x0") {
+                    contextMenu.items[5].checked = false;
+                } else {
+                    contextMenu.items[5].checked = true;
+                    contextMenu.items[4].checked = false;
                 }
-                break
+                break;
         }
         tray.setContextMenu(contextMenu);
     }
-    
 });
 
-const { spawn } = require("child_process");
-
-function run(c) {
-    const ls = spawn(c, {
-        encoding: "utf8",
-        cwd: process.cwd(), // 执行命令路径
-        shell: true, // 使用shell命令
-    });
-
-    // 监听标准输出
-    ls.stdout.on("data", (data) => {
-        return data;
-    });
-
-    // 监听标准错误
-    ls.stderr.on("data", (data) => {
-        show("失败");
-    });
-
-    // 子进程关闭事件
-    ls.on("close", (code) => {
-        console.log(`子进程退出，退出码 ${code}`);
-    });
-}
-
 function show(t) {
+    new Notification({ title: t}).show()
     console.log(t);
 }
+
